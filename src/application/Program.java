cc6bbdeaaf509a8b9bf7c9eab7d299de9eaa37c3
Juliana Ricardo <szric.ju@gmail.com>
2023-03-20 18:47:13 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Supplier;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Supplier sup = new Supplier(1, "Braskem");
+		
+		System.out.println(sup);
+		
+	}
+
+}
