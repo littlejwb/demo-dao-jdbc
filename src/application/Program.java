e922c00a4fecf7cb6061a3d1c963bec3e7613a30
Juliana Ricardo <szric.ju@gmail.com>
2023-03-22 17:31:28 -0300

    Fixing the code

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,9 +34,9 @@ public class Program {
 		}
 		
 		System.out.println("\n- TEST 4: product insert -");
-		//Product newProd = new Product(null, "Toluene", 12.5, "Solvent", sup);
-		//productDao.insert(newProd);
-		//System.out.println("Inserted! New id = " + newProd.getId());
+		Product newProd = new Product(null, "Toluene", 12.5, "Solvent", sup);
+		productDao.insert(newProd);
+		System.out.println("Inserted! New id = " + newProd.getId());
 		
 		System.out.println("\n- TEST 5: product update -");
 		prod = productDao.findById(1);
